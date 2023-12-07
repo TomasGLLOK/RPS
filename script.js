78fc39ba9ba3b68eff17f97736162d29bb0ca083
TomasGLLOK <tomasgll2996@gmail.com>
2023-12-06 22:03:14 -0600

    adding full program

--- script.js
+++ script.js
@@ -6,3 +6,36 @@ function getComputerChoice () {
     let choice = Math.round(Math.random() * 2);
     return myArray[choice];
 }
+
+let pChoice = "Rock";
+let cChoice = getComputerChoice();
+
+function playRound(pChoice, cChoice) {
+    if (pChoice == cChoice) {
+        console.log("Is a tie!");
+    }
+    else if (pChoice=="Rock" && cChoice=="Paper") {
+        console.log("You loose!");
+        console.log("You choose ${pChoice} PC choose ${cChoice}");
+    }
+   else if (pChoice=="Rock" && cChoice=="Scissors") {
+        console.log("You win!");
+        console.log("You choose ${pChoice} PC choose ${cChoice}");
+    }
+    else if (pChoice=="Paper" && cChoice=="Scissors") {
+        console.log("You loose!");
+        console.log("You choose ${pChoice} PC choose ${cChoice}");
+    }
+   else if (pChoice=="Paper" && cChoice=="Rock") {
+        console.log("You win!");
+        console.log("You choose ${pChoice} PC choose ${cChoice}");
+    }
+    else if (pChoice=="Scissors" && cChoice=="Rock") {
+        console.log("You loose!");
+        console.log("You choose ${pChoice} PC choose ${cChoice}");
+    }
+   else if (pChoice=="Scissors" && cChoice=="Paper") {
+        console.log("You win!");
+        console.log("You choose ${pChoice} PC choose ${cChoice}");
+    }
+}
